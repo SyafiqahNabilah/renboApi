80d1134f1f6308e30bdf07ea4a5c575684ad7b3d
Syafiqah Hisham <syafiqahnabilah98@gmail.com>
2026-03-27 16:47:40 +0800

    Merge Owners + Renbo into single User entity - close #12 and #13

--- src/main/java/com/rbms/renbo/repository/userRepository.java
+++ src/main/java/com/rbms/renbo/repository/userRepository.java
@@ -1,18 +1,22 @@
 package com.rbms.renbo.repository;
 
-import com.rbms.renbo.model.User;
+import com.rbms.renbo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-//As interface to user bean
+import java.util.List;
+import java.util.UUID;
 
-/**
- *
- * @author Syafiqah Hisham
- */
 @Repository
-public interface userRepository extends JpaRepository<User, Long> {
-    //provide interface/implement way to authorize
-    User findByEmail(String email);
-    User save(User user);
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query(value = "select * from user where role=?1", nativeQuery = true)
+    List<User> findByRole(String role);
+
+    @Query(value = "update user set status=?1 where userid=?2", nativeQuery = true)
+    void updateStatusUser(String status, UUID id);
+
+    User findUserByEmail(String email);
+
 }
